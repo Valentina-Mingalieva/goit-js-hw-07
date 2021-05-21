afcf4511f75462084ea10a3279817461f729b308
Valentina Mingalieva <valentina.mingalieva@gmail.com>
2021-05-21 12:47:55 +0200

    task 7

--- js/task-07.js
+++ js/task-07.js
@@ -3,4 +3,8 @@
 const input = document.querySelector("#font-size-control");
 const text = document.querySelector("#text");
 
-input.addEventListener("")
+input.addEventListener("input", onInputRange);
+
+function onInputRange(event) {
+    text.style.fontSize = event.currentTarget.value + "px";
+}
